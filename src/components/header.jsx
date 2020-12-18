2c12ac55d43a07229b44b8c0b2d1bba238b5a085
camilo <daniel.sanchez24102001@gmail.com>
2020-12-18 15:05:53 -0500

    applying for mobile and desktop

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,47 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import burger_icon from "../icons/bars-solid.svg";
 
 const Header = (props) => {
+  const [burger, setBurger] = useState(false);
+
   return (
     <React.Fragment>
-      <button className="burger-button">
-        <svg
-          aria-hidden="true"
-          focusable="false"
-          data-prefix="fas"
-          data-icon="bars"
-          className="svg-inline--fa fa-bars fa-w-14"
-          role="img"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 448 512"
-          height="30"
-          width="30"
-        >
-          <path
-            fill="currentColor"
-            d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z"
-          ></path>
-        </svg>
-      </button>
-      <div className="header-nav-dad">
-        <div className="header-nav-children">
-          <div id="header-name">
-            <span>Laboratorios</span>
-          </div>
-          <div id="header-buttons">
-            <div className="button">
-              <Link to="/add-user">Agregar Usuario</Link>
+      {window.screen.width < 420 ? (
+        <Link className="burger-button">
+          <svg
+            aria-hidden="true"
+            focusable="false"
+            data-prefix="fas"
+            data-icon="bars"
+            className="svg-inline--fa fa-bars fa-w-14"
+            role="img"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 448 512"
+            height="30"
+            width="30"
+            onClick={() => setBurger(!burger)}
+            to=""
+          >
+            <path
+              fill="currentColor"
+              d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z"
+            ></path>
+          </svg>
+        </Link>
+      ) : (
+        ""
+      )}
+
+      {burger || window.screen.width > 420 ? (
+        <div className="header-nav-dad">
+          <div className="header-nav-children">
+            <div id="header-name">
+              <span>Laboratorios</span>
             </div>
-            <div className="button">
-              <Link to="/add-user">Ver Reporte</Link>
-            </div>
-            <div className="button">
-              <Link to="/add-user">Buscar Usuario</Link>
+            <div id="center-div"></div>
+            <div id="header-buttons">
+              <div className="button-box">
+                <div className="button">
+                  <Link to="/add-user">Agregar Usuario</Link>
+                </div>
+              </div>
+              <div className="button-box">
+                <div className="button">
+                  <Link to="/add-user">Ver Reporte</Link>
+                </div>
+              </div>
+              <div className="button-box">
+                <div className="button">
+                  <Link to="/add-user">Buscar Usuario</Link>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      ) : (
+        ""
+      )}
     </React.Fragment>
   );
 };
